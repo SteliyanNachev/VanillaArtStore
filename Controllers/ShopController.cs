50f642b3698d825e3877b1789061b467530f27e2
SteliyanNachev <st.g.nachev@gmail.com>
2021-09-09 10:44:42 +0300

    Add project files.

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VanillaArtStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.LayoutSecond = true;
+            return View();
+        }
+    }
+}
