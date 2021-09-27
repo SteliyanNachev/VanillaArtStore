f81cf5e6856acf2312d8c361803059da46d7f2e5
SteliyanNachev <st.g.nachev@gmail.com>
2021-09-27 22:46:06 +0300

    Edit Contact Page

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿namespace VanillaArtStore.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
